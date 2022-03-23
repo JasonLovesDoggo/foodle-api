aea03adaf00c36deebfa1a0efe1380a49cdeaa04
Jason <thebosssg19@gmail.com>
2022-03-23 19:36:43 -0400

    updates/flask fix

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import flask
-from flask import redirect
 
-app = flask.Flask(__name__)
+from flask import redirect,jsonify, json, Flask
+
+# from apiclient.discovery import build
+# from oauth2client.service_account import ServiceAccountCredentials
+# from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+# CORS(app)
+
+with open('version.json', 'r') as vj:
+    number = json.load(vj)
+
 @app.route('/')
 def index():
     return redirect('nasoj.me')
@@ -9,5 +19,37 @@ def index():
 @app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
-    return redirect('nasoj.me/404.html'), 404
+    return redirect('https://nasoj.me/404.html'), 404
+    # CORS(app)
 
+
+
+@app.get('/v1/foodle/version')
+def version():
+    return jsonify(number)
+
+
+@app.get("/v1/foodle/word/daily")
+def search_query_daily():
+    daily_word = 'daily'  # Better Logic
+    return CreateWordResponse(daily_word, 200, 'daily')
+
+
+@app.get("/v1/foodle/word/hourly")
+def search_query_hourly():
+    hourly_word = 'hourly'  # Better Logic
+    return CreateWordResponse(hourly_word, 200, 'hourly')
+
+
+@app.get("/v1/foodle/word/infinite")
+def search_query_infinite():
+    infinite_word = 'infinite'  # Better Logic
+    return CreateWordResponse(infinite_word, 200, 'infinite')
+
+
+@app.route("/v1/foodle/stats/<mode>/<action>", methods=["POST"])  # i know app.post but a plugin im using to debug doesn't
+def statistics(mode: str, action: str):
+    if mode.lower == 'daily':
+        if action.lower() == 'win':
+            pass
+    return f'{mode} x {action}'
