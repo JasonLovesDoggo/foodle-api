ba0a01c5cbb995e596be2d627447d45ca9738909
Jason <thebosssg19@gmail.com>
2022-04-10 18:13:36 -0400

    it should work now (env variables)

--- send_data.py
+++ send_data.py
@@ -1,4 +1,6 @@
+import json
 from os import getenv
+from os.path import exists
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
 import requests
@@ -6,42 +8,45 @@ from datetime import datetime
 
 from utils import get_daily
 
-hourmap = {0: '12 AM',
-           1: '1 AM',
-           2: '2 AM',
-           3: '3 AM',
-           4: '4 AM',
-           5: '5 AM',
-           6: '6 AM',
-           7: '7 AM',
-           8: '8 AM',
-           9: '9 AM',
-           10: '10 AM',
-           11: '11 AM',
-           12: '12 PM',
-           13: '1 PM',
-           14: '2 PM',
-           15: '3 PM',
-           16: '4 PM',
-           17: '5 PM',
-           18: '6 PM',
-           19: '7 PM',
-           20: '8 PM',
-           21: '9 PM',
-           22: '10 PM',
-           23: '11 PM'}
+hourmap = {'0': '12 AM',
+           '1': '1 AM',
+           '2': '2 AM',
+           '3': '3 AM',
+           '4': '4 AM',
+           '5': '5 AM',
+           '6': '6 AM',
+           '7': '7 AM',
+           '8': '8 AM',
+           '9': '9 AM',
+           '10': '10 AM',
+           '11': '11 AM',
+           '12': '12 PM',
+           '13': '1 PM',
+           '14': '2 PM',
+           '15': '3 PM',
+           '16': '4 PM',
+           '17': '5 PM',
+           '18': '6 PM',
+           '19': '7 PM',
+           '20': '8 PM',
+           '21': '9 PM',
+           '22': '10 PM',
+           '23': '11 PM'}
 
+with open('data/generated_words.json', 'r') as gw:
+    gen_words = json.load(gw)
 
 class StatsWrapper:
     def __init__(self):
-        load_dotenv()
+        if exists('.env'):
+            load_dotenv()
         self.PHONENUM = getenv('phonenum')
         self.API_KEY = getenv('apikey')
         self.message = 'pee pee poo poo'
         self.date = datetime.today()  # .strftime('%A')
 
-    def HourReplacment(self, hour: int):  # im sure there is a builtin for this, but I don't know it
-        return hourmap[hour]
+    def HourReplacment(self, hour: str):  # im sure there is a builtin for this, but I don't know it
+        return hourmap[str(hour)]
 
     def send(self):
         API_URL = f"https://api.callmebot.com/signal/send.php?phone=+{self.PHONENUM}&apikey={self.API_KEY}&text={quote_plus(self.message)}"
@@ -66,13 +71,15 @@ Hourly words ↓	↓	↓
 
     def get_hourlys(self) -> list[tuple]:
         # word/ hour
-        raw_words = [('grate', 0), ('pizza', 1), ('thyme', 2), ('scone', 3), ('pizza', 4), ('syrup', 5), ('quite', 6),
+        raw_words = gen_words['hourly']
+        print(raw_words)
+        raw_words_2 = [('grate', 0), ('pizza', 1), ('thyme', 2), ('scone', 3), ('pizza', 4), ('syrup', 5), ('quite', 6),
                      ('pasta', 7), ('salsa', 8), ('patty', 9), ('morry', 10), ('grill', 11), ('pecan', 12),
                      ('fresh', 13),
                      ('vodka', 14), ('kebab', 15), ('tangy', 16), ('sugar', 17), ('aroma', 18), ('shake', 19),
                      ('brine', 20), ('spill', 21), ('sweet', 22), ('mince', 23)]
         words = []
-        for word, hour in raw_words:
+        for hour, word in raw_words.items():
             words.append(f'{word} at {self.HourReplacment(hour)}')
         return words
 
