3752a4e69103985541c72cb54788aaab32a59061
Jason <thebosssg19@gmail.com>
2022-04-10 20:26:40 -0400

    refactored / cleaned up utils.py /: moved some functions around

--- utils.py
+++ utils.py
@@ -23,6 +23,32 @@ with open('data/generated_words.json', 'r') as gw:
     gen_words = json.load(gw)
     log.info('loaded Generated words')
 
+def HourReplacment(hour: str):  # im sure there is a builtin for this, but I don't know it
+    hourmap = {'0': '12 AM',
+               '1': '1 AM',
+               '2': '2 AM',
+               '3': '3 AM',
+               '4': '4 AM',
+               '5': '5 AM',
+               '6': '6 AM',
+               '7': '7 AM',
+               '8': '8 AM',
+               '9': '9 AM',
+               '10': '10 AM',
+               '11': '11 AM',
+               '12': '12 PM',
+               '13': '1 PM',
+               '14': '2 PM',
+               '15': '3 PM',
+               '16': '4 PM',
+               '17': '5 PM',
+               '18': '6 PM',
+               '19': '7 PM',
+               '20': '8 PM',
+               '21': '9 PM',
+               '22': '10 PM',
+               '23': '11 PM'}
+    return hourmap[str(hour)]
 
 @lru_cache(maxsize=1)
 def get_daily():
@@ -41,10 +67,6 @@ def get_word(word: str):
     if word in wordlist:
         return jsonify(word_data[word])
     return CreateErrorResponse('word not in wordlist', 404)
-
-
-def CreateWordResponse(word: str, status_code: int):
-    return jsonify({'status': status_code, 'word': word, })
 
 
 def CreateErrorResponse(error: str, status_code: int):
